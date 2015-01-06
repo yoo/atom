412670be54d76687158f34b63c69f3f20dd7df1b
Johann Weging <johann@weging.net>
2015-01-06 23:31:31 +0100

    Make disposables work.

--- disposable.go
+++ disposable.go
@@ -5,12 +5,18 @@ import "github.com/gopherjs/gopherjs/js"
 // Disposable doc at: https://atom.io/docs/api/v0.165.0/Disposable
 type Disposable interface {
 	Dispose()
+	getJsObject() js.Object
 }
 
 type disposableImpl struct {
 	o js.Object
 }
 
+// Dispose doc at: https://atom.io/docs/api/v0.165.0/Disposable#instance-dispose
 func (d *disposableImpl) Dispose() {
 	d.o.Call("dispose")
+}
+
+func (d *disposableImpl) getJsObject() js.Object {
+	return d.o
 }
